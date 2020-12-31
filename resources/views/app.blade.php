3d77f5fe32e2de3b8a4560c219e1fac75ecc66ac
elhakimdev <abdulelhakim68@gmail.com>
2021-01-01 05:35:21 +0700

    change all markpu html

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{asset('js/app.js')}}"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
     <style>
@@ -14,53 +15,82 @@
          font-family: 'IBM Plex Sans', sans-serif;   
         }
     </style>
+    {{-- <script type="text/javascript" src="{{ asset('js/app.js') }}"></script> --}}
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 <body class="antialiased bg-gray-100">
-    {{-- top nav --}}
-    <section id="top-nav">
-        <nav class="top-nav bg-white shadow py-2">
-            <div class="mx-5 grid gap-x-5 grid-cols-12">
-                <div class="col-span-2">
-                    <div class="flex flex-row items-center justify-start">
-                        <a href="#" class="">
-                            <i class="fab fa-angular fa-3x text-blue-700 hover:text-blue-900"></i> 
-                        </a>
-                        <a href="#" class="ml-6 text-3xl subpixel-antialiased font-regular text-blue-700 hover:text-blue-900 hover:text-bold">IBM<span><strong class="text-3xl font-light">Plex</strong></span></a>
+
+    <section class="shadow bg-white md:fixed md:top-0 w-full">
+        <nav class="md:flex items-center mx-5 p-2">
+            <div class="md:flex-1">
+                <div class="flex flex-row items-center justify-between md:justify-start">
+
+                    <a href="#" class="">
+                        <i class="fab fa-angular fa-3x text-blue-700 hover:text-blue-900"></i> 
+                    </a>
+
+                    <a href="#" class="hidden md:block ml-6 text-3xl subpixel-antialiased font-regular text-blue-700 hover:text-blue-900 hover:text-bold">IBM<span><strong class="text-3xl font-light">Plex</strong></span></a>
+
+                    <div class="md:hidden">
+                        <button 
+                        onclick="toggleNav()"
+                        type="button"
+                            class="p-3 text-blue-600"
+                            aria-label="toggle menu">
+                            <i class="fas fa-align-justify"></i>
+                        </button>
                     </div>
                 </div>
-                <div class="col-span-10">
-                    <div class="flex flex-row items-center justify-center">
-                        <div class="w-2/3 relative flex flex-row">
+            </div>
+            <div class="hidden md:flex md:flex-grow md:relative">
+                <input type="text" name="search" id="search" class="w-full py-1 my-3 pl-10 bg-gray-100 rounded border border-blue-200 focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 focus:border-transparent" placeholder="Search anything here ....">
 
-                            <input type="text" name="search" id="search" class="w-full py-1 my-3 pl-10 bg-gray-100 rounded border border-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" placeholder="Search anything here ....">
+                <svg class="w-4 h-4 md:absolute inset-3 mt-2 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            </div>
+            <div id="menu" class="hidden transition-all md:block mt-3 md:flex-1 py-2">
+                <div class="flex flex-col md:flex md:flex-row md:items-center md:justify-end">
+                    <a href="#" class="p-3 md:p-0 hover:bg-gray-200 md:hover:bg-transparent">
 
-                            <svg class="w-4 h-4 absolute inset-3 mt-2 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                        </div>
-                        <div class="w-1/3 flex flex-row justify-end items-center">
-                            <a href="#" class="ml-3">
-                                <i class="fas fa-bell text-gray-400 hover:text-blue-700 text-xl ml-3"></i>
-                            </a>
+                        <i class="fas fa-bell text-gray-400 hover:text-blue-700 text-xl ml--3 md:ml-3"></i>
+                        
+                        <span class="ml-3 font-semibold text-gray-400 hover:text-blue-700 md:hidden">Notifications</span>
+                    </a>
 
-                            <a href="#" class="ml-3">
-                                <i class="fas fa-cog text-gray-400 hover:text-blue-700 text-xl ml-3"></i>
-                            </a>
-                            
-                            <a href="#" class="ml-3">
-                                <i class="fas fa-user text-gray-400 hover:text-blue-700 text-xl ml-3"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                    <a href="#" class="p-3 md:p-0 hover:bg-gray-200 md:hover:bg-transparent">
+
+                        <i class="fas fa-cog text-gray-400 hover:text-blue-700 text-xl ml--3 md:ml-3"></i>
+                        
+                        <span class="ml-3 font-semibold text-gray-400 hover:text-blue-700 md:hidden">Settings</span>
+                    </a>
+                    
+                    <a href="#" class="p-3 md:p-0 hover:bg-gray-200 md:hover:bg-transparent">
+                        
+                        <i class="fas fa-user text-gray-400 hover:text-blue-700 text-xl ml--3 md:ml-3"></i>
+                        
+                        <span class="ml-3 font-semibold text-gray-400 hover:text-blue-700 md:hidden">Accounts</span>
+                    </a> 
+                </div>               
             </div>
         </nav>
     </section>
 
+
     {{-- main section --}}
-    <section>
-        {{-- body --}}
-        <div class="body grid grid-cols-12">
-            {{-- sidebar --}}
-            <div class="sidebar col-span-2 py-1 min-h-screen">
+    <sectiont id="content">
+
+        {{-- body grid 12 colom --}}
+        <div class="body md:grid md:grid-cols-12 md:mt-16 md:py-2">
+
+            {{-- toggler menu --}}
+            <div class="md:hidden flex flex-row mt-3 py-3 items center justify-between ml-5">
+                <button class="p-3">
+                    <i class="fas fa-align-justify"></i>
+                    <span class="ml-5">Menu</span>
+                </button>
+            </div>
+
+            {{-- sidebar 2 kolom hidden on mobile--}}
+            <div class="hidden md:block sidebar flex flex-col md:col-span-2 md:py-1 md:min-h-screen">
                 {{-- group menu 1 --}}
                 <div class="mb-2">
                         <a href="#" class="flex items-center py-3 px-6 bg-white border-l-4 border-blue-600 hover:bg-gray-300 transition duration-500 ease-in-out">
@@ -125,22 +155,121 @@
             </div>
             {{-- end sidebar --}}
 
-            {{-- main container --}}
-            <div class="layout col-span-10 min-h-screen shadow">
+            {{-- main container  10 kolom--}}
+            <div class="layout md:col-span-10 md:min-h-screen md:shadow">
                 <div class="mb-2 text-gray-500">
-                    <div class="border-b-2 py-3 flex flex-row items-center justify-between">
-                        <div class="ml-4">
-                            <h1>Dashboard</h1>
-                        </div>
-                        <div class="breadcrumb mr-4">
+                    {{-- breadcrumb --}}
 
-                        </div>
+                    {{-- top card --}}
+                    <div class="grid gap-y-3 mx-5 md:grid-cols-12 md:gap-x-3 md:mx-6 md:my-6">
+                        {{-- <div class="grid grid-cols-12 gap-x-4"> --}}
+                            <div class="col-span-3 shadow bg-white">    
+                                <div class="px-4 py-5 sm:p-6">
+                                    <dl>
+                                        <dt class="text-sm leading-5 font-medium text-gray-500 truncate">
+                                            Valeur de la transaction
+                                        </dt>
+                                        <dd class="mt-1 text-3xl leading-9 font-semibold text-blue-900">
+                                            $ 5,749,480
+                                        </dd>
+                                        <dd class="text-gray-500 font-semibold">
+                                            <span>
+                                                500
+                                                <span class="text-xs">
+                                                    .000
+                                                </span>
+                                            </span>
+                                            BTC
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>
+                            <div class="col-span-3 shadow bg-white">    
+                                <div class="px-4 py-5 sm:p-6">
+                                    <dl>
+                                        <dt class="text-sm leading-5 font-medium text-gray-500 truncate">
+                                            Valeur de la transaction
+                                        </dt>
+                                        <dd class="mt-1 text-3xl leading-9 font-semibold text-blue-900">
+                                            $ 5,749,480
+                                        </dd>
+                                        <dd class="text-gray-500 font-semibold">
+                                            <span>
+                                                500
+                                                <span class="text-xs">
+                                                    .000
+                                                </span>
+                                            </span>
+                                            BTC
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>
+                            <div class="col-span-3 shadow bg-white">    
+                                <div class="px-4 py-5 sm:p-6">
+                                    <dl>
+                                        <dt class="text-sm leading-5 font-medium text-gray-500 truncate">
+                                            Valeur de la transaction
+                                        </dt>
+                                        <dd class="mt-1 text-3xl leading-9 font-semibold text-blue-900">
+                                            $ 5,749,480
+                                        </dd>
+                                        <dd class="text-gray-500 font-semibold">
+                                            <span>
+                                                500
+                                                <span class="text-xs">
+                                                    .000
+                                                </span>
+                                            </span>
+                                            BTC
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>
+                            <div class="col-span-3 shadow bg-white">    
+                                <div class="px-4 py-5 sm:p-6">
+                                    <dl>
+                                        <dt class="text-sm leading-5 font-medium text-gray-500 truncate">
+                                            Valeur de la transaction
+                                        </dt>
+                                        <dd class="mt-1 text-3xl leading-9 font-semibold text-blue-900">
+                                            $ 5,749,480
+                                        </dd>
+                                        <dd class="text-gray-500 font-semibold">
+                                            <span>
+                                                500
+                                                <span class="text-xs">
+                                                    .000
+                                                </span>
+                                            </span>
+                                            BTC
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>  
+                        {{-- </div> --}}
                     </div>
-                </div>
+                    {{-- end top card --}}
             </div>
             {{-- end --}}
+
         </div>
 
     </section>
+    {{-- end main --}}
+
+    <script>
+        function toggleNav() {
+            if (document.getElementById("menu").classList.contains("hidden")) {
+                document.getElementById("menu").classList.remove("hidden");
+                document.getElementById("menu"),classList.add("show");
+            } else {
+                if (document.getElementById("menu").classList.contains("show")) {
+                    document.getElementById("menu").classList.remove("show");
+                    document.getElementById("menu"),classList.add("hidden");
+                }
+            }
+        }
+    </script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('pages.dashboard');
+// });
+Route::get('/login', function () {
+    return view('auth.login');
+});
+Route::get('/', function(){
     return view('app');
 });
